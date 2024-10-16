7324451a22083e625d5a454dc98fda684cbee309
Muhammad-Shoaib1 <shoaibshinwari97@gmail.com>
2024-10-16 13:16:05 +0500

    Completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const popUp = document.getElementById("popup");
+
+function openPopup() {
+  popUp.classList.add("open-popup");
+}
+
+function closePopup() {
+  popUp.classList.remove("open-popup");
+}
